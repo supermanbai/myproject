50a0e675e8fe665feba12581e3a99bebba2c5d75
supermanbai <webbaiemail@163.com>
2020-05-14 18:30:31 +0800

    2020.05.14

---
+++ constants.js
@@ -0,0 +1,10 @@
+const GLOBAL = {
+    API: "/csdn/",
+    // 导航页签显示文字，根据路由来找
+    breadKey: [
+        { key: 'dashboard', cnName: '首页', enName: '' },
+        { key: 'classAdm', cnName: '课程管理', enName: '' },
+        { key: 'memberAdm', cnName: '会员管理', enName: '' },
+        { key: 'orderAdm', cnName: '订单管理', enName: '' },
+    ]
+};
